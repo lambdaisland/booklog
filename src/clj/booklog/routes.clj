8b2fb53fd3e967b10b156d915ad56ddac0699581
Arne Brasseur <arne@arnebrasseur.net>
2017-03-12 16:56:08 +0100

    Demonstrate dependency error
    Happens when requiring buddy.auth.backends.

--- src/clj/booklog/routes.clj
+++ src/clj/booklog/routes.clj
@@ -6,40 +6,14 @@
             [buddy.auth.backends :as buddy-backends]
             [buddy.hashers :as hashers]))
 
-(defn login-authenticate
-  "Check request username and password against authdata
-  username and passwords.
-  On successful authentication, set appropriate user
-  into the session and redirect to the value of
-  (:next (:query-params request)). On failed
-  authentication, renders the login page."
-  [request]
-  (let [username (get-in request [:form-params "username"])
-        password (get-in request [:form-params "password"])
-        session (:session request)
-        found-password (get authdata (keyword username))]
-    (if (and found-password (= found-password password))
-      (let [next-url (get-in request [:query-params :next] "/")
-            updated-session (assoc session :identity (keyword username))]
-        (-> (redirect next-url)
-            (assoc :session updated-session)))
-      (let [content (slurp (io/resource "login.html"))]
-        (render content request)))))
+#_
+(def buddy-backend
+  (buddy-backends/session))
 
 (defn ok-html [h]
   (-> (str (layout h))
       response
       (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-
-
-
-#_
-(with-open [db (open-database "./baking-db")]
-  (let [ingredients (open-hashmap db "ingredient-hashmap")]
-    (put! ingredients :apple-pie [:flour :butter :sugar :apples])
-    ;;=> [:flour :butter :sugar :apples]
-    (update! ingredients :apple-pie #(conj % :cinnamon))))
-;;=> [:flour :butter :sugar :apples :cinnamon]
 
 (defn app-routes [{:keys [spicerack] :as endpoint}]
   (routes
@@ -47,16 +21,16 @@
    (GET "/" _
      (ok-html (login-form)))
 
-   (GET "/register" [username password]
-     (let [users (open-hashmap (:db spicerack) "users")]
-       (put! users username (hashers/derive password))
-       (ok-html (register-form))))
+   (GET "/register" _
+     (ok-html (register-form)))
 
    (POST "/login" {:keys [session]
                    {:keys [username password]} :params}
 
      (ok-html (layout ())))
 
-   (resources "/")))
+   (POST "/register" [username password]
+     #_(let [users (open-hashmap (:db spicerack) "usaers")]
+         (put! users username (hashers/derive password))))
 
-(buddy-backends/session)
+   (resources "/")))
