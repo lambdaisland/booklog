882da07cfd66aca1d2082dec7d6df3565ea7fdf6
Arne Brasseur <arne@arnebrasseur.net>
2017-08-16 15:43:41 +0200

    Finish two integration tests

--- test/clj/booklog/acceptance_test.clj
+++ test/clj/booklog/acceptance_test.clj
@@ -1,4 +1,35 @@
 (ns booklog.acceptance-test
   (:require [clojure.test :refer :all]
             [booklog.test-helper :refer :all]
-            [sparkledriver.core :as sd :refer [click! send-text!]]))
+            [sparkledriver.core :as sd :refer
+             [click! send-text!]]))
+
+(use-fixtures :once wrap-test-system wrap-browser)
+(use-fixtures :each wrap-clear-db wrap-clear-cookies)
+
+(deftest sign-up-test
+  (fetch! (app-url "/"))
+  (click! (find-by-css "a.log-in"))
+  (is (= (current-path) "/login"))
+
+  (click! (find-by-xpath "//a[text()='Register instead']"))
+  (is (= (current-path) "/register"))
+
+  (send-text! (find-by-css "#username") "Arne")
+  (send-text! (find-by-css "#password") "sekrit")
+  (click! (find-by-css "input[type=submit]"))
+
+  (is (re-find #"Thanks for registering! Please log in" (page-text))))
+
+(deftest login-test
+  (load-db! {"users" {"Arne" "bcrypt+sha512$e0cf0035cd4f616d180119d2c3cd4c12$12$a841ed436714247677f0342268ede7aa9e681b71d6060b46"}})
+
+  (fetch! (app-url "/login"))
+  (send-text! (find-by-css "#username") "Arne")
+  (send-text! (find-by-css "#password") "sekrit")
+  (click! (find-by-css "input[type=submit]"))
+
+  (is (re-find #"Login successful, welcome back!" (page-text))))
+
+(comment
+  (run-tests))
--- test/clj/booklog/test_helper.clj
+++ test/clj/booklog/test_helper.clj
@@ -9,6 +9,10 @@
 
 (def test-http-port 59800)
 
+(def ^:dynamic *browser* nil)
+
+(def ^:dynamic *system* nil)
+
 (defn test-config []
   {:http-port test-http-port
    :db-path (util/temp-file-name "test" "db")})
@@ -18,6 +22,62 @@
   [tests]
   (let [config (test-config)
         system (-> config app/app-system component/start)]
-    (tests)
+    (binding [*system* system]
+      (tests))
     (component/stop system)
     (io/delete-file (:db-path config))))
+
+(defn wrap-clear-db
+  "Test fixture which clears the database before each test."
+  [test]
+  (sc/clear-db! (:spicerack *system*))
+  (test))
+
+(defn wrap-browser
+  "A fixture function which binds *browser* to a new browser instance."
+  [tests]
+  (sd/with-browser [browser (sd/make-browser)]
+    (binding [*browser* browser]
+      (tests))))
+
+(defn wrap-clear-cookies [test]
+  (sd/delete-all-cookies! *browser*)
+  (test))
+
+(defn fetch!
+  ([url]
+   (sd/fetch! *browser* url))
+  ([browser url]
+   (sd/fetch! browser url)))
+
+(defn find-by-css
+  ([css]
+   (sd/find-by-css *browser* css))
+  ([browser css]
+   (sd/find-by-css browser css)))
+
+(defn find-by-xpath
+  ([xpath]
+   (sd/find-by-xpath *browser* xpath))
+  ([browser xpath]
+   (sd/find-by-xpath browser xpath)))
+
+(defn page-text
+  ([]
+   (sd/page-text *browser*))
+  ([browser]
+   (sd/page-text)))
+
+(defn app-url [path]
+  (assoc (uri/uri "http://localhost")
+         :path path
+         :port test-http-port))
+
+(defn current-path []
+  (:path (uri/uri (sd/current-url *browser*))))
+
+(defn dump-db []
+  (sc/dump-db (:spicerack *system*)))
+
+(defn load-db! [data]
+  (sc/load-db! (:spicerack *system*) data))
